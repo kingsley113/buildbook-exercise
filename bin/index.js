3c16b1138eb6878465c618bcfef2cdad0ce82d13
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-23 15:37:06 -0800

    implement reading of spotify.json

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,16 @@
 #! /usr/bin/env node
-
 console.log("Hello World!");
+
+// Get command line arguments into constants
+const [origFile, changeFile, outputFile] = process.argv.slice(2);
+// console.log(origFile, changeFile, outputFile);
+
+// Parse original JSON data
+const data = require(`../data/${origFile}`);
+console.log(data);
+// Parse changes JSON data
+const changes = require(`../data/${changeFile}`);
+
+// Modify data as required
+
+// output modified data into new file
