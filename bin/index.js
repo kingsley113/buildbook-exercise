d1bd7067eb3b3930bc58ee743ad26e09b9fb0443
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-23 16:05:28 -0800

    Rough out switch statement

--- bin/index.js
+++ bin/index.js
@@ -7,10 +7,28 @@ const [origFile, changeFile, outputFile] = process.argv.slice(2);
 
 // Parse original JSON data
 const data = require(`../data/${origFile}`);
-console.log(data);
 // Parse changes JSON data
+// Random assortment of changes created in changes.json file, per project requirements
 const changes = require(`../data/${changeFile}`);
+// console.log(changes);
+
+for (const key in changes) {
+  // console.log(changes[key]);
+  switch (key) {
+    case "add_song_to_playlist":
+      addSongToPlaylist(changes[key]);
+    case "create_new_playlist":
+      createNewPlaylist(changes[key]);
+    case "remove_existing_paylist":
+      removePlaylist(changes[key]);
+    default:
+  }
+}
 
 // Modify data as required
 
 // output modified data into new file
+
+// Define functions for modifying data
+
+// Add song to existing playlist
